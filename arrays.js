cb30fd7c039d5accccf52b2903266585d0096ea9
Daniel Bailey <daniel4423@gmail.com>
2019-01-30 19:50:48 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = [
+  'snickers', 
+  'hundred grand', 
+  'kitkat', 
+  'skittles'
+];
+
+function addElementToBeginningOfArray(theArray, element) {
+  return [...theArray, element];
+}
+
+destructilyAddElementToBeginningOfArray(myArray, element) {
+  var array.unshift(element)
+}
